2f4bb992c451480371357dc5514418c3a30be713
espinachs <espinachs@gmail.com>
2024-08-20 14:16:42 +0000

    clase 4

---
+++ actividad2.py
@@ -0,0 +1,26 @@
+
+
+#"ej1"
+tiempo = float(input (" Tiempo "))
+Velocidad = float(input (" Velocidad "))
+Distancia = Velocidad * tiempo
+print(Distancia)
+
+#"ej2"
+
+Num1 = float(input ("nota1 "))
+Num2 = float(input (" nota2 "))
+Num3 = float(input (" nota3"))
+Num4 = Num1 + Num2 + Num3
+result = Num4/3
+print(result)
+
+#"ej3"
+
+precio = float(input (" precio? "))
+Calc1 = precio/100
+Descuento = int(input (" Descuento ? "))
+Calc2 = Calc1 * Descuento
+PrecioFinal = precio - Calc2
+print(" El valor sera= ",PrecioFinal)
+
